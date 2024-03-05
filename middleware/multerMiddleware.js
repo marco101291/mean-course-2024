43e3ffcb96c510be7604ca2ed9afa5c178829c33
Marco Antonio Lievano Escobar <marco.lievano@digitalonus.com>
2024-03-04 20:00:31 -0600

    fixed updateUser

--- middleware/multerMiddleware.js
+++ middleware/multerMiddleware.js
@@ -1,15 +1,27 @@
 import multer from "multer";
+import DataURIParser from "datauri/parser.js";
+import path from 'path';
 
-const storage = multer.diskStorage({
-    destination: (req, file, cb) =>{
-        cb(null, 'public/uploads');
-    },
-    filename: (req, file, cb) =>{
-        const fileName = file.originalname;
-        cb(null, fileName)
-    }
-})
+// const storage = multer.diskStorage({
+//     destination: (req, file, cb) =>{
+//         cb(null, 'public/uploads');
+//     },
+//     filename: (req, file, cb) =>{
+//         const fileName = file.originalname;
+//         cb(null, fileName)
+//     }
+// })
 
+const storage = multer.memoryStorage()
 const upload = multer({storage});
+
+const parser = new DataURIParser();
+
+export const formatImage = (file) =>{
+    const fileExtension = path.extname(file.originalname).toString()
+    return parser.format(fileExtension, file.buffer).content;
+};
+
+
 
 export default upload;
